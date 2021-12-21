8f3e6bf8521e333828cfd61c380ca3510bee74af
success899 <success899@naver.com>
2021-12-22 01:05:37 +0900

    header nav btn / image serving url / account detail page design update

--- jh_1/urls.py
+++ jh_1/urls.py
@@ -1,5 +1,4 @@
-"""jh_1 URL Configuration
-
+"""gis_drf_1 URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
@@ -13,12 +12,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
 
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
